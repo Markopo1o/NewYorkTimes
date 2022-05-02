214bd4f6936d9ff9373c99c853f534852a54f290
Markopo1o <32136014+Markopo1o@users.noreply.github.com>
2022-05-02 16:51:19 +0200

    a

--- app/src/main/java/hu/jm/newyorktimes/ui/list/ListFragment.kt
+++ app/src/main/java/hu/jm/newyorktimes/ui/list/ListFragment.kt
@@ -9,11 +9,13 @@ import androidx.appcompat.widget.SearchView
 import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import coil.ImageLoader
 import coil.request.ImageRequest
 import coil.request.SuccessResult
+import hu.jm.newyorktimes.BuildConfig
 import hu.jm.newyorktimes.R
 import hu.jm.newyorktimes.databinding.FragmentListBinding
 import hu.jm.newyorktimes.model.News
@@ -21,9 +23,7 @@ import hu.jm.newyorktimes.ui.list.adapter.NewsListAdapter
 import kotlinx.coroutines.launch
 
 class ListFragment : Fragment(), SearchView.OnQueryTextListener{
-
     private val viewModel: ListViewModel by viewModels()
-    private lateinit var pic: Bitmap
     private val adapter = NewsListAdapter()
     private var _binding: FragmentListBinding? = null
     private val binding
@@ -35,11 +35,14 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener{
         _binding = FragmentListBinding.inflate(inflater,container,false)
         return binding.root
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.getLiveData().observe(this
+        ) { result -> render(result) }
+    }
     override fun onResume() {
         super.onResume()
         setHasOptionsMenu(true)
-        viewModel.getLiveData().observe(this
-        ) { result -> render(result) }
         val recyclerView = _binding!!.recyclerview
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -68,8 +71,8 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener{
                 lifecycleScope.launch {
                     viewModel.deleteAllUsers()
                     while (i < result.data.results.size){
-                        pic = if(result.data.results[i].media.isEmpty()){
-                            getBitmap("https://developer.nytimes.com/logos/logo?v=1600101622804")
+                         val pic = if(result.data.results[i].media.isEmpty()){
+                            getBitmap(BuildConfig.DEFPIC_URL)
                         }else{
                             getBitmap(result.data.results[i].media[0].mediaMetadata!![0]!!.url)
                         }
@@ -96,7 +99,7 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener{
     }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater){
         inflater.inflate(R.menu.search_menu, menu)
-        inflater.inflate(R.menu.more_menu, menu)
+        //inflater.inflate(R.menu.more_menu, menu)
         val search = menu.findItem(R.id.menu_search)
         val searchView = search?.actionView as? SearchView
         searchView?.setOnQueryTextListener(this)
